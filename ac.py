61c5401ca8f26dccfcbbb5e37127cc1a043f55bf
zj23333 <19121010MSI_c>
2024-04-17 11:19:26 -0400

    change data to san, trace_interval to 3, action = F

--- ac.py
+++ ac.py
@@ -122,7 +122,7 @@ def train(cfg,envs):
                                                 trace_length=100,
                                                 trace_interval=3,
                                                 is_full_observation=False,
-                                                is_full_action=False)
+                                                is_full_action=True)
     eval_env = BatchMigrationEnv(env_eval_parameters)  # envs是训练用的，eval_env是测试用的
     ### 
     n_states  = envs.observation_space  # 在我的env里，这个observation_space和action_space都是普通的int
@@ -255,7 +255,7 @@ if __name__ == '__main__':
                                                 trace_length=100,
                                                 trace_interval=3,
                                                 is_full_observation=False,
-                                                is_full_action=False)
+                                                is_full_action=True)
     # envs = [make_envs(cfg.env_name) for i in range(cfg.n_envs)]
     envs = BatchMigrationEnv(env_default_parameters)# SubprocVecEnv(envs) 
     rewards,ma_rewards = train(cfg,envs)
